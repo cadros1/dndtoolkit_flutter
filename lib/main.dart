ad146bed63dac5f480ca0a34deb272e0f553de8f
cadros1 <cadros1@outlook.com>
2025-12-15 01:28:16 +0800

    配置夜间模式，设置主题跟随系统

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,33 @@ class DnDToolkitApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const seedColor = Colors.deepPurple;
+
     return MaterialApp(
       title: 'DnD Toolkit',
+      // 配置亮色主题 (Light Mode)
       theme: ThemeData(
-        // 开启 Material 3 风格
         useMaterial3: true,
-        // 设置主题种子颜色
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: seedColor,
+          brightness: Brightness.light, // 明确指定为亮色
+        ),
       ),
+
+      // 配置暗色主题 (Dark Mode)
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: seedColor,
+          brightness: Brightness.dark, // 明确指定为暗色
+        ),
+      ),
+
+      // 设置主题模式跟随系统 (默认就是 system，写出来更清晰)
+      // system: 跟随系统设置
+      // light: 强制亮色
+      // dark: 强制暗色
+      themeMode: ThemeMode.system, 
       scaffoldMessengerKey: SnackBarService.scaffoldMessengerKey,
       // 指定首页
       home: const MainScreen(),
