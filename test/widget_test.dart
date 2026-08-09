c9a2e43311a923d3bd6441982b981867f946ce2c
cadros1 <cadros1@outlook.com>
2026-08-09 22:11:31 +0800

    test: 更新应用启动页断言

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,10 @@
-import 'dart:ui';
-
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:dndtoolkit_flutter/main.dart';
 
 void main() {
-  testWidgets('app starts on character management', (tester) async {
+  testWidgets('应用启动后显示角色页和移动端导航', (tester) async {
     tester.view.physicalSize = const Size(390, 844);
     tester.view.devicePixelRatio = 1;
     addTearDown(() {
@@ -15,8 +14,14 @@ void main() {
 
     await tester.pumpWidget(const DnDToolkitApp());
 
-    expect(find.text('角色管理'), findsOneWidget);
+    expect(find.byType(NavigationBar), findsOneWidget);
     expect(find.text('角色'), findsOneWidget);
     expect(find.text('冒险'), findsOneWidget);
+    expect(find.text('更多'), findsOneWidget);
+
+    final navigationBar = tester.widget<NavigationBar>(
+      find.byType(NavigationBar),
+    );
+    expect(navigationBar.selectedIndex, 0);
   });
 }
